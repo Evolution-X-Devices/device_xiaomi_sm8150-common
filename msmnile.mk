82416fec8b23288fd9a234514d58ca97be51c72d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-02-04 22:03:11 +0100

    sm8150-common: Move ramdisk fstab target to Soong
    Change-Id: I1e3c56a6ab44a3e64021e23d5e6e3d8e972071b4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -434,7 +434,8 @@ endif
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    fstab.qcom_ramdisk
+    fstab.qcom.ramdisk \
+    fstab.qcom.vendor_ramdisk
 
 PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
