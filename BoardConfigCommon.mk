c1cb511ffcbb84e811af85547879468e7dde47b8
Bruno Martins <bgcngm@gmail.com>
2025-06-10 18:44:42 +0200

    sm8150-common: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,9 +76,6 @@ ifeq ($(TARGET_HAS_UDFPS),true)
 TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/xiaomi:libudfps_extension.xiaomi
 endif
 
-# Init
-TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_msmnile
-
 # Kernel
 ifeq ($(TARGET_IS_VAB),true)
 BOARD_BOOT_HEADER_VERSION := 3
