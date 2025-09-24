e33606373e4bd681ba6f1892f36f09e6abe244af
firebird11 <hbgassel@gmail.com>
2025-09-24 22:56:48 +0200

    sm8150-common: Move Powerhint to device
    Change-Id: I82d8d8bf49dfeb4c6adb1b669fb2a1b8e2115e20
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -391,9 +391,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
 PRODUCT_SOONG_NAMESPACES += \
     bootable/deprecated-ota \
     hardware/google/interfaces \
