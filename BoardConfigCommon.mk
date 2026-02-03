DerpFest-Devices/device_lenovo_sm8150-common

sm8150-common: Override kernel BPF version
Change-Id: I3c581f3120b6258b3b45020c5ba12f6bf5b0ee27

BoardConfigCommon.mk
@@ -162,6 +162,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_ODM_PROP += $(DEVICE_PATH_COMMON)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH_COMMON)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH_COMMON)/vendor.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH_COMMON)/product.prop
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888

Evolution-X-Devices/device_xiaomi_sm8150-common

BoardConfigCommon.mk
@@ -153,6 +153,7 @@ TARGET_BOARD_PLATFORM := msmnile
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 ifneq ($(TARGET_IS_TABLET),true)