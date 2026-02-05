cbe7e3355b30679bb975824ef4888c11fdad89dc
firebird11 <hbgassel@gmail.com>
2026-02-05 21:05:25 +0100

    sm8150-common: Add Livedisplay
        * From LineageOS/android_device_xiaomi_sm6150-common
    
    Change-Id: I4cdd38a88cfb781311719f793263ac6605d5dc37

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -95,6 +95,14 @@ TARGET_KERNEL_CONFIG := \
     vendor/xiaomi/sm8150-common.config
 TARGET_KERNEL_NO_GCC := true
 
+# Livedisplay
+SOONG_CONFIG_NAMESPACES += xiaomiSm8150Vars
+SOONG_CONFIG_xiaomiSm8150Vars += \
+    livedisplay_support_anti_flicker \
+    livedisplay_support_sunlight_enhancement
+SOONG_CONFIG_xiaomiSm8150Vars_livedisplay_support_anti_flicker ?= true
+SOONG_CONFIG_xiaomiSm8150Vars_livedisplay_support_sunlight_enhancement ?= true
+
 # Media
 TARGET_USES_ION := true
 
