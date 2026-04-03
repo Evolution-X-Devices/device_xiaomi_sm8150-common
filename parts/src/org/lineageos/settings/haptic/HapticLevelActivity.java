momenabdulrazek/android_device_xiaomi_sm8150-common

sm8150-common: parts: Introduce haptic feedback level adjustment
Since our kernel exposes adjustable level, lets add
a simple UI for it.

Change-Id: I35577682f97e4aad880e455423bf51f6cbe1641b

Change-Id: I50c9db84f4b7bdcfe3b915757f0779387854ffc5
Signed-off-by: firebird11 <hbgassel@gmail.com>

parts/src/org/lineageos/settings/haptic/HapticLevelActivity.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.lineageos.settings.haptic;
+
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
+public class HapticLevelActivity extends CollapsingToolbarBaseActivity {
+
+    private final String TAG_HAPTIC = "haptic_level";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(
+                com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                new HapticLevelFragment(), TAG_HAPTIC)
+                .commit();
+    }
+}

parts/src/org/lineageos/settings/haptic/HapticLevelFragment.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.lineageos.settings.haptic;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+
+import org.lineageos.settings.R;
+import org.lineageos.settings.utils.FileUtils;
+import org.lineageos.settings.widget.SeekBarPreference;
+
+public class HapticLevelFragment extends PreferenceFragment implements OnPreferenceChangeListener {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.haptic_settings);
+
+        final SeekBarPreference mHapticLevel = (SeekBarPreference) findPreference(HapticUtils.PREF_LEVEL);
+        if (FileUtils.fileExists(HapticUtils.PATH_LEVEL)) {
+            mHapticLevel.setEnabled(true);
+            mHapticLevel.setOnPreferenceChangeListener(this);
+        } else {
+            mHapticLevel.setSummary(R.string.haptic_level_summary_incompatible);
+            mHapticLevel.setEnabled(false);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = LayoutInflater.from(getContext()).inflate(R.layout.haptic, container, false);
+        ((ViewGroup) view).addView(super.onCreateView(inflater, container, savedInstanceState));
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (HapticUtils.PREF_LEVEL.equals(preference.getKey())) {
+            HapticUtils.applyLevel(getContext(), (int) newValue, true);
+        }
+
+        return true;
+    }
+}

Evolution-X-Devices/device_xiaomi_sm8150-common

parts/src/org/lineageos/settings/haptic/HapticLevelActivity.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.lineageos.settings.haptic;
+
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
+public class HapticLevelActivity extends CollapsingToolbarBaseActivity {
+
+    private final String TAG_HAPTIC = "haptic_level";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportFragmentManager().beginTransaction().replace(
+                com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                new HapticLevelFragment(), TAG_HAPTIC)
+                .commit();
+    }
+}

parts/src/org/lineageos/settings/haptic/HapticLevelFragment.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.lineageos.settings.haptic;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settingslib.widget.SettingsBasePreferenceFragment;
+
+import org.lineageos.settings.R;
+import org.lineageos.settings.utils.FileUtils;
+import org.lineageos.settings.widget.SeekBarPreference;
+
+public class HapticLevelFragment extends SettingsBasePreferenceFragment implements OnPreferenceChangeListener {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.haptic_settings, rootKey);
+
+        final SeekBarPreference mHapticLevel = (SeekBarPreference) findPreference(HapticUtils.PREF_LEVEL);
+        if (FileUtils.fileExists(HapticUtils.PATH_LEVEL)) {
+            mHapticLevel.setEnabled(true);
+            mHapticLevel.setOnPreferenceChangeListener(this);
+        } else {
+            mHapticLevel.setSummary(R.string.haptic_level_summary_incompatible);
+            mHapticLevel.setEnabled(false);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = LayoutInflater.from(getContext()).inflate(R.layout.haptic, container, false);
+        ((ViewGroup) view).addView(super.onCreateView(inflater, container, savedInstanceState));
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (HapticUtils.PREF_LEVEL.equals(preference.getKey())) {
+            HapticUtils.applyLevel(getContext(), (int) newValue, true);
+        }
+
+        return true;
+    }
+}